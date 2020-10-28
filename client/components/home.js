2553b2fde12eb056b1fac4cc614d68051b1773b0
Vladoid <vladserkez@gmail.com>
2020-10-28 18:16:13 +0300

    fix div in home

--- client/components/home.js
+++ client/components/home.js
@@ -39,6 +39,9 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Dashboard</div>
+          <div id="title">Main</div>
+          <div id="title">Profile</div>
           <Switch>
             <Route exact path="/dashboard" component={() => <Dashboard />} />
             <Route exact path="/dashboard/main" component={() => <Main />} />
