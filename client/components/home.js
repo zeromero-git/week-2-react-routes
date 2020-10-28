939226a39bbc4f7663a15d545c444b74431e2da2
Roman <spiderr.roma@gmail.com>
2020-10-28 16:10:53 +0200

    afas

--- client/components/home.js
+++ client/components/home.js
@@ -4,11 +4,11 @@ import Header from './header'
 
 const Home = () => {
   return (
-    <div id="title">
-      Dashboard
+    <div>
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Dashboard</div>
           <Switch>
             <Route exact path="/dashboard" component={() => <Dashboard />} />
             <Route exact path="/dashboard/main" component={() => <Main />} />
@@ -22,17 +22,14 @@ const Home = () => {
 
 const Dashboard = () => {
   return (
-    <div id="title">
-      Dashboard
-      <ul>
-        <li>
-          <Link to="/dashboard/main">Go To Main</Link>
-        </li>
-        <li>
-          <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile</Link>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li>
+        <Link to="/dashboard/main">Go To Main</Link>
+      </li>
+      <li>
+        <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile</Link>
+      </li>
+    </ul>
   )
 }
 
