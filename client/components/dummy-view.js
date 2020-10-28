79fa62321d3364e28fece4d51a9bfe26395d3118
Roman <spiderr.roma@gmail.com>
2020-10-28 15:50:31 +0200

    working vertsion

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import Head from './head'
 
 const Dummy = () => {
@@ -9,6 +9,7 @@ const Dummy = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           This is dummy component
+          <Link to="/dashboard"> Go to Dashboard </Link>
         </div>
       </div>
     </div>
--- client/components/home.js
+++ client/components/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Switch, Route, Link, useParams } from 'react-router-dom'
 import Header from './header'
 
 const Home = () => {
@@ -7,9 +8,63 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          <Switch>
+            <Route exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+            <Route exact path="/dashboard/profile/:userId" component={() => <Profile />} />
+          </Switch>
         </div>
       </div>
+    </div>
+  )
+}
+
+const Dashboard = () => {
+  return (
+    <div id="title">
+      Dashboard
+      <ul>
+        <li>
+          <Link to="/dashboard/main">Go To Main</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const Main = () => {
+  return (
+    <div id="title">
+      Main
+      <ul>
+        <li>
+          <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Go To Root</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const Profile = () => {
+  const { userId } = useParams()
+  return (
+    <div id="title">
+      Profile
+      <ul>
+        <li>
+          <Link to="/dashboard">Go To Root</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/main">Go To Main</Link>
+        </li>
+      </ul>
+      <div id="username">{userId}</div>
     </div>
   )
 }
