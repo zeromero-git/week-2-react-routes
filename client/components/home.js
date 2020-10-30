912ab067722a71fd1621d540779b8a8f9496028a
Vladoid <vladserkez@gmail.com>
2020-10-31 01:34:18 +0300

    fix home.js

--- client/components/home.js
+++ client/components/home.js
@@ -13,15 +13,13 @@ const Home = () => {
     )
   }
 
-  const Main = () => {
-    return (
-      <div id="title">
-        Main
-        <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile </Link>
-        <Link to="/dashboard">Go To Root</Link>
-      </div>
-    )
-  }
+  const Main = () => (
+    <div id="title">
+      Main
+      <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile </Link>
+      <Link to="/dashboard">Go To Root</Link>
+    </div>
+  )
 
   const Profile = () => {
     const { userId } = useParams()
@@ -31,15 +29,13 @@ const Home = () => {
         <div id="username">{userId}</div>
         <Link to="/dashboard">Go To Root </Link>
         <Link to="/dashboard/main">Go To Main</Link>
-      </div>
-    )
-  }
+    </div>
+  )
   return (
     <div>
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Dashboard</div>
           <Switch>
             <Route exact path="/dashboard" component={() => <Dashboard />} />
             <Route exact path="/dashboard/main" component={() => <Main />} />
