486b474985a91171008b1a86979931f234a063f5
Vladoid <vladserkez@gmail.com>
2020-11-11 02:05:30 +0300

    new dz

--- client/components/home.js
+++ client/components/home.js
@@ -1,9 +1,138 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+import ReactMarkdown from 'react-markdown'
+import { history } from '../redux'
 import Header from './header'
-import Dashboard from './dashboard'
-import Main from './main'
-import Profile from './profile'
+
+const InputName = (props) => {
+  const [value, setValue] = useState()
+  const onChange = (e) => {
+    const newValue = e.target.value
+    setValue(newValue)
+    props.onChange(newValue)
+  }
+  return (
+    <input
+      id="input-field"
+      value={value}
+      onChange={onChange}
+      className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+      type="text"
+      placeholder="userName"
+      STYLE="color: yellow"
+    />
+  )
+}
+
+const Search = () => {
+  const [name, setName] = useState()
+  const onInputChange = (text) => {
+    setName(text)
+  }
+  function goName() {
+    history.push(`/${name}`)
+  }
+  return (
+    <form className="w-full max-w-sm">
+      <InputName onChange={onInputChange} />
+      <div className="flex items-center border-b border-teal-500 py-2">
+        <button
+          id="search-button"
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          type="button"
+          onClick={goName}
+        >
+          Seacrh
+        </button>
+      </div>
+    </form>
+  )
+}
+
+const UserName = () => {
+  const { userName } = useParams()
+  const Headers = () => {
+    return (
+      <div>
+        <div id="repositoryName">{userName}</div>
+        <div id="go-back">
+          <Link to="/">Go Back</Link>
+        </div>
+      </div>
+    )
+  }
+  const User = (props) => {
+    const user = props
+    return (
+      <div key={user.name}>
+        <div style={{ width: '250px' }}>
+          <Link to={`/${userName}/${user.name}`}>{user.name}</Link>
+        </div>
+      </div>
+    )
+  }
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios.get(`https://api.github.com/users/${userName}/repos`).then((it) => {
+      setUsers(it.data)
+    })
+    return () => {}
+  }, [userName])
+  return (
+    <div>
+      <Headers />
+      {users.map((user) => {
+        // eslint-disable-next-line
+        return <User key={user.name} {...user} />
+      })}
+    </div>
+  )
+}
+
+const UserRepo = () => {
+  const { userName } = useParams()
+  const { repositoryName } = useParams()
+  const [repo, setRepo] = useState({})
+  const [mark, setMark] = useState({})
+  const Headers = () => {
+    return (
+      <div>
+        <div id="repositoryName">{userName}</div>
+        <div id="go-back">
+          <Link to="/">Go Root</Link>
+        </div>
+        <div id="go-repository-list">
+          <Link to={`/${userName}`}>Go Back</Link>
+        </div>
+      </div>
+    )
+  }
+  useEffect(
+    () => {
+      axios.get(`https://api.github.com/repos/${userName}/${repositoryName}/readme`).then((it) => {
+        setRepo(it.data)
+      })
+      return () => {}
+    },
+    { userName, repositoryName }
+  )
+  useEffect(
+    () => {
+      axios.get(`${repo.download_url}`).then((it) => {
+        setMark(it.data)
+      })
+      return () => {}
+    },
+    { repo }
+  )
+  return (
+    <div>
+      <Headers />
+      <ReactMarkdown source={mark} />
+    </div>
+  )
+}
 
 const Home = () => {
   return (
@@ -12,9 +141,9 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Switch>
-            <Route exact path="/dashboard" component={() => <Dashboard />} />
-            <Route exact path="/dashboard/main" component={() => <Main />} />
-            <Route exact path="/dashboard/profile/:userId" component={() => <Profile />} />
+            <Route exact path="/" component={() => <Search />} />
+            <Route exact path="/:userName" component={() => <UserName />} />
+            <Route exact path="/:userName/:repositoryName" component={() => <UserRepo />} />
           </Switch>
         </div>
       </div>
@@ -25,30 +154,3 @@ const Home = () => {
 Home.propTypes = {}
 
 export default React.memo(Home)
-
-// const { userId } = useParams()
-
-// const Dashboard = () => (
-//   <div id="title">
-//     Dashboard
-//     <Link to="/dashboard/main">Go To Main </Link>
-//     <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile</Link>
-//   </div>
-// )
-
-// const Main = () => (
-//   <div id="title">
-//     Main
-//     <Link to="/dashboard/profile/3b21ca93-2f6b-45ee-92c6-0872fdaf45a7">Go To Profile </Link>
-//     <Link to="/dashboard">Go To Root</Link>
-//   </div>
-// )
-
-// const Profile = () => (
-//   <div id="title">
-//     Profile
-//     <div id="username">{userId}</div>
-//     <Link to="/dashboard">Go To Root </Link>
-//     <Link to="/dashboard/main">Go To Main</Link>
-//   </div>
-// )
